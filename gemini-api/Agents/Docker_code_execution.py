9acc9ab45d0faace258f98f004d2f42f18e985ea
GODCREATOR333 <haripreetham.jntuh@gmail.com>
2024-12-24 07:19:45 +0530

    adde docker code execution

--- gemini-api/Agents/Docker_code_execution.py
+++ gemini-api/Agents/Docker_code_execution.py
@@ -4,6 +4,7 @@ import tempfile
 from dotenv import load_dotenv
 import os 
 import google.generativeai as genai
+import logging
 
 
 load_dotenv()
@@ -14,10 +15,9 @@ genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
 
 # Create a temporary directory to store the code files.
 temp_dir = tempfile.TemporaryDirectory()
-
 # Create a Docker command line code executor.
 executor = DockerCommandLineCodeExecutor(
-    image="python:3.12-slim",  # Execute code using the given docker image name.
+    image="python",  # Execute code using the given docker image name.
     timeout=10,  # Timeout for each code execution in seconds.
     work_dir=temp_dir.name,  # Use the temporary directory to store the code files.
 )
@@ -42,7 +42,8 @@ In the following cases, suggest python code (in a python coding block) or shell 
 Solve the task step by step if you need to. If a plan is not provided, explain your plan first. Be clear which step uses code, and which step uses your language skill.
 When using code, you must indicate the script type in the code block. The user cannot provide any other feedback or perform any other action beyond executing the code you suggest. The user can't modify your code. So do not suggest incomplete code which requires users to modify. Don't use a code block if it's not intended to be executed by the user.
 If you want the user to save the code in a file before executing it, put # filename: <filename> inside the code block as the first line. Don't include multiple code blocks in one response. Do not ask users to copy and paste the result. Instead, use 'print' function for the output when relevant. Check the execution result returned by the user.
-If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
+If the result indicates there is an error, fix the error and output the code again
+. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
 When you find an answer, verify the answer carefully. Include verifiable evidence in your response if possible.
 Reply 'TERMINATE' in the end when everything is done.
 """
