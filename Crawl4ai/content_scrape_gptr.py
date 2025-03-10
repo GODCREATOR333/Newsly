63b129b09a5875d1f41a5d417f8a1d7e5a3a462b
GODCREATOR333 <haripreetham.jntuh@gmail.com>
2025-03-11 02:05:28 +0530

    gptr is working for indivual content articles

--- Crawl4ai/content_scrape_gptr.py
+++ Crawl4ai/content_scrape_gptr.py
@@ -1,5 +1,26 @@
 from gpt_researcher import GPTResearcher
 import asyncio
+from gpt_researcher.config.config import Config
+from gpt_researcher.utils.llm import create_chat_completion
+import asyncio
+from dotenv import load_dotenv
+load_dotenv()
+
+async def main():
+    cfg = Config()
+
+    try:
+        report = await create_chat_completion(
+            model=cfg.smart_llm_model,
+            messages = [{"role": "user", "content": "sup?"}],
+            temperature=0.35,
+            llm_provider=cfg.smart_llm_provider,
+            stream=True,
+            max_tokens=cfg.smart_token_limit,
+            llm_kwargs=cfg.llm_kwargs
+        )
+    except Exception as e:
+        print(f"Error in calling LLM: {e}")
 
 async def fetch_report(query):
     """
@@ -18,5 +39,8 @@ async def generate_research_report(query):
     print(report)
 
 if __name__ == "__main__":
-    QUERY = "What happened in the latest burning man floods?"
+    QUERY = "Apple unveils a souped-up and more expensive version of its lowest priced iPhone. Link{https:/apnews.com/article/apple-low-cost-iphone-artificial-intelligence-1fc43f8b839d7995763fbe939f5de290}"
     asyncio.run(generate_research_report(query=QUERY))
+
+# Run the async function
+asyncio.run(main())
